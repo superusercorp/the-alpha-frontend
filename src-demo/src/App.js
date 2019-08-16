64a20647a56a9e9b1ddc5d0795731ca39ce42180
bw <bw@MacBook-Pro-3.local>
2019-08-15 19:39:04 -0500

    Adding header component, working deploy to firebase

--- src-demo/src/App.js
+++ src-demo/src/App.js
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { PostList, PostShow, PostCreate, PostEdit } from "./posts";
 import { UserList, UserShow, UserCreate, UserEdit } from "./users";
+import AppHeader from "./header";
 import { Admin, Resource } from "react-admin";
+import 'bootstrap/dist/css/bootstrap.css';
+
 import {
   FirebaseRealTimeSaga,
   FirebaseDataProvider,
@@ -27,30 +30,12 @@ const firebaseRealtime = FirebaseRealTimeSaga(dataProvider, options);
 class App extends React.Component {
   render() {
     return (
-      <Admin
-        loginPage={CustomLoginPage} 
-        dataProvider={dataProvider}
-        authProvider={authProvider}
-        customSagas={[firebaseRealtime]}
-      >
-        <Resource
-          name="posts"
-          list={PostList}
-          show={PostShow}
-          create={PostCreate}
-          edit={PostEdit}
-        />
-        <Resource
-          name="users"
-          icon={UserIcon}
-          list={UserList}
-          show={UserShow}
-          create={UserCreate}
-          edit={UserEdit}
-        />
-      </Admin>
+      <AppHeader></AppHeader>
     );
   }
 }
 
+
+
 export default App;
+
--- src-demo/src/CustomLoginPage.js
+++ src-demo/src/CustomLoginPage.js
@@ -1,11 +1,17 @@
 // LoginPage.js
 import React from "react";
 import { Login, LoginForm } from "react-admin";
+import AppHeader from "./App";
 
 const CustomLoginForm = props => (
+
+
+  
   <div>
+    
     <div style={{fontFamily: "monospace", marginLeft: '15px'}}>
-      <p>Username: anthony@zappin.com</p>
+   
+      <p>DFUsername: anthony@zappin.com</p>
       <p>Password: zappin</p>
     </div>
     <LoginForm {...props} />
