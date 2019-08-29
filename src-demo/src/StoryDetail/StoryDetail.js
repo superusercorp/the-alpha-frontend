87e010021efcaf244ffd318638ed74f6ae500d2e
bw <bw@MacBook-Pro-4.local>
2019-08-29 11:23:51 -0500

    adding html parser for article body

--- src-demo/src/StoryDetail/StoryDetail.js
+++ src-demo/src/StoryDetail/StoryDetail.js
@@ -1,9 +1,11 @@
 import * as React from "react";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 class StoryDetail extends React.Component {
     render() {
         const article = Object.values(this.props.location.state)
         console.log(JSON.stringify(article))
+        const htmlBody = ReactHtmlParser(article[0].body);
 
         return (
             <div class="section">
@@ -13,7 +15,7 @@ class StoryDetail extends React.Component {
                         <div class="section-row sticky-container">
                             <div class="main-post">
                                 <h3>{article[0].title}</h3>
-                                <p>{article[0].body}</p>
+                                <p>{htmlBody}</p>
 
                                 {/* <p>Do you like Cheese Whiz? Spray tan? Fake eyelashes? That's what is Lorem Ipsum to many—it rubs them the wrong way, all the way. It's unreal, uncanny, makes you wonder if something is wrong, it seems to seek your attention for all the wrong reasons. Usually, we prefer the real thing, wine without sulfur based preservatives, real butter, not margarine, and so we'd like our layouts and designs to be filled with real words, with thoughts that count, information that has value. </p>
                                 <p>The toppings you may chose for that TV dinner pizza slice when you forgot to shop for foods, the paint you may slap on your face to impress the new boss is your business. But what about your daily bread? Design comps, layouts, wireframes—will your clients accept that you go about things the facile way? Authorities in our business will tell in no uncertain terms that Lorem Ipsum is that huge, huge no no to forswear forever. Not so fast, I'd say, there are some redeeming factors in favor of greeking text, as its use is merely the symptom of a worse problem to take into consideration.</p>
