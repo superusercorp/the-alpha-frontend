a663b2b1c766c4b2d4662c7013d067bd75c8e1bb
bw <bw@MacBook-Pro-4.local>
2019-10-20 23:07:56 -0500

    [ads] wrapping it up

---
+++ src-demo/src/Ads/Ads.js
@@ -0,0 +1,16 @@
+import React  from 'react';
+
+function Ads() {
+    return (
+    <div>
+        <div class="col-md-4" style={{display: 'inline-block', 'margin-left': '800px', 'margin-top': '-100px'}}>
+            <div class="aside-widget text-center">
+                 <img class="img-responsive" src="/img/ad-1.jpg" alt=""/>
+            <h3>MOST READ</h3>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Ads; 
--- src-demo/src/StoryDetail/StoryDetail.js
+++ src-demo/src/StoryDetail/StoryDetail.js
@@ -1,11 +1,11 @@
 import * as React from "react";
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
+import ReactHtmlParser from 'react-html-parser';
+import Ads from "../Ads/Ads.js"
 
 const StoryDetail = (props) => {
     const title = props.location.state.title
     const body = props.location.state.title
     const htmlBody = ReactHtmlParser(body);
-
     return (
         <div class="section">
             <div class="container">
@@ -13,8 +13,8 @@ const StoryDetail = (props) => {
                     <div class="col-md-8">
                         <div class="section-row sticky-container">
                             <div class="main-post">
-                                <h3>{title}</h3>
-                                <p>{htmlBody}</p>
+                                <h1>{title}</h1>
+                                <h3>{htmlBody}</h3>
                             </div>
                             <div class="post-shares sticky-shares">
                                 <a href="#" class="share-facebook"><i class="fa fa-facebook"></i></a>
@@ -25,6 +25,7 @@ const StoryDetail = (props) => {
                                 <a href="#"><i class="fa fa-envelope"></i></a>
                             </div>
                         </div>
+                        <Ads/>
                     </div>
                 </div>
             </div>
