cc3842f5f0acf6e96058dd514a5ffacb76fcad56
bw <bw@MacBook-Pro-4.local>
2019-10-21 01:09:31 -0500

    [social share buttons] adding shares with package.json update

--- src-demo/src/StoryDetail/StoryDetail.js
+++ src-demo/src/StoryDetail/StoryDetail.js
@@ -1,11 +1,17 @@
 import * as React from "react";
 import ReactHtmlParser from 'react-html-parser';
 import Ads from "../Ads/Ads.js"
+import { TwitterShareButton, FacebookShareButton, EmailShareButton, TwitterIcon, FacebookIcon, EmailIcon, FacebookShareCount } from 'react-share';
+import { Facebook, Twitter } from 'react-sharingbuttons'
+import {
+    useLocation,
+} from "react-router-dom";
 
 const StoryDetail = (props) => {
     const title = props.location.state.title
     const body = props.location.state.title
     const htmlBody = ReactHtmlParser(body);
+    let location = window.location.href
     return (
         <div class="section">
             <div class="container">
@@ -17,12 +23,22 @@ const StoryDetail = (props) => {
                                 <h3>{htmlBody}</h3>
                             </div>
                             <div class="post-shares sticky-shares">
-                                <a href="#" class="share-facebook"><i class="fa fa-facebook"></i></a>
-                                <a href="#" class="share-twitter"><i class="fa fa-twitter"></i></a>
-                                <a href="#" class="share-google-plus"><i class="fa fa-google-plus"></i></a>
-                                <a href="#" class="share-pinterest"><i class="fa fa-pinterest"></i></a>
-                                <a href="#" class="share-linkedin"><i class="fa fa-linkedin"></i></a>
-                                <a href="#"><i class="fa fa-envelope"></i></a>
+                                {/* <a href="#" class="share-facebook"> */}
+                                    {/* <i class="fa fa-facebook"></i> */}
+                                <FacebookIcon url={location} quote={title} size={32} round={true} />
+                                <Facebook url={location} shareText={title} />
+                                <p></p>
+                                    {/* </a>  */}
+                                <a href="#" class="share-twitter">
+                                    {/* <i class="fa fa-twitter"></i> */}
+                                <TwitterShareButton  quote={title} className="fa fa-twitte" url={location} size={32} round={true} />
+                                <TwitterIcon  quote={title} className="fa fa-twitte" url={location} size={32} round={true} />
+                                </a>
+                                <a href="#">
+                                    {/* <i class="fa fa-envelope"></i> */}
+                                <EmailShareButton  quote={title} className="fa fa-twitte" url={location} size={32} round={true} />
+                                <EmailIcon  quote={title} className="fa fa-twitte" url={location} size={32} round={true} />
+                                </a>
                             </div>
                         </div>
                         <Ads/>
